ce53f3464f3b5fbdc5dc25add102f6af31ad35d4
Fahad Qazi <fahadqazi@gmail.com>
2023-08-05 21:59:59 +0100

    Update server handle

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -10,3 +10,11 @@ func (a *application) healthcheckHandler(w http.ResponseWriter, r *http.Request)
 	fmt.Fprintf(w, "environtment: %s\n", a.config.env)
 	fmt.Fprintf(w, "version: %s\n", version)
 }
+
+func (a *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create movie")
+}
+
+func (a *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "show movie")
+}
